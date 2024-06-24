5c56b100793f2a350df79556cd9745da71cf22ec
Tushar More <158036610+Tushar-Project-Space@users.noreply.github.com>
2024-06-24 10:42:01 +0530

    mid-level commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,24 @@ function Navbar({ className }: { className?: string }) {
   return (
     <div  className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
      <Menu setActive={setActive}>
-      <Link href={"#"}>
+      <Link href={"/"}>
         <MenuItem setActive={setActive} active={active} item="Home">
+        
+        </MenuItem>
+      </Link>
+
+      <MenuItem setActive={setActive} active={active} item="Our Courses">
+      <div className="flex flex-col space-y-4 test-sm">
+      <HoveredLink href="/courses">All Courses</HoveredLink>
+      <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+      <HoveredLink href="/courses">Advance Composition</HoveredLink>
+      <HoveredLink href="/courses">SongWritting</HoveredLink>
+      <HoveredLink href="/courses">Music Production</HoveredLink>
+      </div>
+      </MenuItem>
+
+      <Link href={"/contact"}>
+        <MenuItem setActive={setActive} active={active} item="Contact Us">
         
         </MenuItem>
       </Link>
