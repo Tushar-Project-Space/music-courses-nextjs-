7c1604c7e59c37b227b01d88113c9e58c8121df9
Tushar More <158036610+Tushar-Project-Space@users.noreply.github.com>
2024-06-28 11:20:31 +0530

    Final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 
 import WhyChooseUs from "@/components/WhyChooseUs";
-
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
       <WhyChooseUs />
       <MusicSchoolTestimonials />
       <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
