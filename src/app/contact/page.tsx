fbd97e91cbe00a353280425c256442c4ed9951ee
Tushar More <158036610+Tushar-Project-Space@users.noreply.github.com>
2025-07-10 17:34:40 +0530

    change in contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,25 +1,23 @@
-'use client';
+"use client";
 
-import React, { FormEvent, useState } from 'react';
-import { BackgroundBeams } from '@/components/ui/background-beams';
+import React, { FormEvent, useState } from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 function MusicSchoolContactUs() {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('Submitted:', { email, message });
+    console.log("Submitted:", { email, message });
   };
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
-  
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
-      
+
       <div className="max-w-2xl mx-auto p-4 relative z-10">
-        {' '}
-       
+        {" "}
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
           Contact Us
         </h1>
@@ -49,7 +47,7 @@ function MusicSchoolContactUs() {
             type="submit"
             className="px-6 py-2 rounded-lg bg-teal-500 text-white font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
           >
-            Send Message
+            Send Message to me
           </button>
         </form>
       </div>
